7dd7c85574a74b677c82d15238f17fb729278b20
Defu95 <wangdefu95@gmail.com>
2018-10-17 16:46:25 +0800

    leetcode 338

--- Counting_Bits.java
+++ Counting_Bits.java
@@ -5,9 +5,20 @@ public class Counting_Bits{
 			System.out.print(test[i]+" ");
 	}
     public static int[] countBits(int num) {
-		int[] res=new int[num+1];
-		for(int i=0;i<res.length;i++)
-			res[i]=Integer.bitCount(i);
-		return res;
+		// int[] res=new int[num+1];
+		// for(int i=0;i<res.length;i++)
+		// 	res[i]=Integer.bitCount(i);
+		// return res;
+/*
+<<:左移运算符，num << 1,相当于num乘以2
+>>:右移运算符，num >> 1,相当于num除以2
+>>>:无符号右移，忽略符号位，空位都以0补齐
+*/
+        int[] dp=new int[num+1];
+        for(int i=1;i<=num;i++){
+            if(i%2==0) dp[i]=dp[i>>1];
+            else dp[i]=dp[i>>1]+1;
+        }
+        return dp;
     }
 }
