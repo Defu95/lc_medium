fab2054773c1ffe4d9c08bbb6cb3843310a52629
Defu95 <wangdefu95@gmail.com>
2018-10-22 11:03:51 +0800

    leetcode 763

--- Partition_Labels.java
+++ Partition_Labels.java
@@ -8,8 +8,18 @@ public class Partition_Labels{
 	}
     public static List<Integer> partitionLabels(String S) {
         List<Integer> res=new ArrayList<>();
-        res.add(9);
-        res.add(7);
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<S.length();i++){
+        	map.put(S.charAt(i),i);
+        }
+        int maxlen=0,start=0;
+        for(int i=0;i<S.length();i++){
+            maxlen=Math.max(maxlen,map.get(S.charAt(i)));
+            if(maxlen==i){
+                res.add(i-start+1);
+                start=i+1;
+            }
+        }
         return res;
     }
 }
