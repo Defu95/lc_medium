20eb1194ce54e3d08b2ecb82bdc6cc9ef59e17f7
Defu95 <wangdefu95@gmail.com>
2018-11-14 21:42:19 +0800

    leetcode 82

--- Remove_Duplicates_from_Sorted_List_II.java
+++ Remove_Duplicates_from_Sorted_List_II.java
@@ -6,28 +6,47 @@ class ListNode {
  
 public class Remove_Duplicates_from_Sorted_List_II{
 	public static void main(String[] args) {
-		ListNode head=new ListNode(5),cur=head;
-		head.next=new ListNode(3);
-		head=head.next;
-		head.next=new ListNode(3);
-		head=head.next;
-		head.next=new ListNode(2);
-		head=head.next;	
-		head.next=new ListNode(2);
+		ListNode head=new ListNode(1),cur=head;
+		head.next=new ListNode(1);
 		head=head.next;
 		head.next=new ListNode(1);
+		head=head.next;
+		head.next=new ListNode(2);
+		// head=head.next;	
+		// head.next=new ListNode(2);
+		head=head.next;
+		head.next=new ListNode(3);
+		ListNode p=cur;
 		while(cur!=null){
 			System.out.print(cur.val+" ");
 			cur=cur.next;
 		}
+		ListNode res=deleteDuplicates(p);
+		System.out.println();
+		while(res!=null){
+			System.out.print(res.val+" ");
+			res=res.next;
+		}
 	}
-    public ListNode deleteDuplicates(ListNode head) {
-        ListNode tmp=head,p=head,cur=tmp;
-        while(p.next!=null){
-        	if(p.val==p.next.val){
-        		p.next=p.next.next;
+    public static ListNode deleteDuplicates(ListNode head) {
+    	if(head==null||head.next==null)
+    		return head;
+        ListNode tmp=new ListNode(-1);
+        tmp.next=null;
+        ListNode p=head,cur=tmp;
+        while(p!=null){
+        	if(p.next!=null&&p.val==p.next.val){ //
+        		while(p.next!=null&&p.val==p.next.val){  //p.next!=null&&
+        			p=p.next;
+        		}
+        		p=p.next;
+        		tmp.next=p;
+        	}else{
+        		tmp.next=p;
+        		tmp=p;
+        		p=p.next;
         	}
         }
-
+        return cur.next;
     }
 }
