f17e8e30c1738b60d860829d3a2d059947218eb1
Defu95 <wangdefu95@gmail.com>
2018-12-12 21:06:36 +0800

    nowcoder

--- PrintMinNumber.java
+++ PrintMinNumber.java
@@ -1,3 +1,12 @@
 public class PrintMinNumber{
-	
+	public static void main(String[] args) {
+		int[] numbers={3,2,231};
+		System.out.println(printminnumber(numbers));
+	}
+
+	public static String printminnumber(int [] numbers) {
+		if(numbers.length<=0)
+			return null;
+		
+    }
 }
