ad7c68d5cc84b507867df7614cd6d9d69e36a761
Defu95 <wangdefu95@gmail.com>
2018-12-13 21:44:45 +0800

    newcoder FindNumsAppearOnce

---
+++ FindNumsAppearOnce.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class FindNumsAppearOnce{
+	public static void main(String[] args) {
+		int[] array={2,3,2,12,2,3,21};
+		int[] num1=new int[1];
+		int[] num2=new int[1];
+		findnumsappearonce(array,num1,num2);
+		System.out.println(num1[0]);
+		System.out.println(num2[0]);
+	}
+
+	public static void findnumsappearonce(int [] array,int num1[] , int num2[]){
+		HashSet<Integer> tmp=new HashSet<>();
+		for(int num:array){
+			if(tmp.contains(num))
+				tmp.remove(num);
+			else
+				tmp.add(num);
+		}
+		Iterator<Integer> it=tmp.iterator();
+		int flag=-1;
+		while(it.hasNext()){
+			int in=it.next();
+			if(flag==-1){
+				num1[0]=in;
+				flag=1;
+			}else
+			num2[0]=in;
+		}
+	}
+}
