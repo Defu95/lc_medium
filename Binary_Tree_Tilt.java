3a77dc96978859c12a0418477e4805eca689ecd3
Defu95 <wangdefu95@gmail.com>
2019-02-20 15:43:47 +0800

    1

---
+++ Binary_Tree_Tilt.java
@@ -0,0 +1,12 @@
+public class Binary_Tree_Tilt{
+	static class TreeNode{
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x){ val = x;}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
