a16e2554b4da5329c152d884130851ac7169b87d
Defu95 <wangdefu95@gmail.com>
2019-02-22 10:10:26 +0800

    **

--- Binary_Tree_Tilt.java
+++ Binary_Tree_Tilt.java
@@ -7,6 +7,19 @@ public class Binary_Tree_Tilt{
 	}
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		TreeNode t4=new TreeNode(4);
+		TreeNode t5=new TreeNode(5);
+		TreeNode t2=new TreeNode(2);
+		TreeNode t3=new TreeNode(3);
+		t2.left=t4;
+		t3.right=t5;
+		t1.left=t2;
+		t1.right=t3;
+		
+
+	}
+
+	public static int findTilt(TreeNode root){
+		
 	}
 }
