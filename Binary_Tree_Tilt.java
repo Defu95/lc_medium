7b853450b8ed6ffb36c248431e67c1ce8e3f6249
Defu95 <wangdefu95@gmail.com>
2019-02-22 11:24:35 +0800

    leetcode 563 good

--- Binary_Tree_Tilt.java
+++ Binary_Tree_Tilt.java
@@ -11,15 +11,29 @@ public class Binary_Tree_Tilt{
 		TreeNode t5=new TreeNode(5);
 		TreeNode t2=new TreeNode(2);
 		TreeNode t3=new TreeNode(3);
+		TreeNode t1=new TreeNode(1);
 		t2.left=t4;
 		t3.right=t5;
 		t1.left=t2;
 		t1.right=t3;
-		
-
+		//      1
+		//   2     3
+		// 4          5
+		// 4+5+((3+5)-(4+2))=11
+		System.out.println(findTilt(t1));
 	}
 
 	public static int findTilt(TreeNode root){
-		
+		int res=0;
+		if(root==null)
+			return res;
+		res=Math.abs(sumsubtree(root.left)-sumsubtree(root.right));
+		return res+findTilt(root.left)+findTilt(root.right);
+	}
+
+	public static int sumsubtree(TreeNode root){
+		if(root==null)
+			return 0;
+		return root.val+sumsubtree(root.left)+sumsubtree(root.right);
 	}
 }
