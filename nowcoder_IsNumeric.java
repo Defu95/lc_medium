9ae91a720cd5e0a6305fa852299ec4c482abe8ef
Defu95 <wangdefu95@gmail.com>
2019-03-01 11:52:35 +0800

    nowcoder

--- nowcoder_IsNumeric.java
+++ nowcoder_IsNumeric.java
@@ -1,11 +1,11 @@
 import java.util.*;
 public class nowcoder_IsNumeric{
 	public static void main(String[] args) {
-		char[] str1={'+','1','0'};
+		char[] str1={'+','5','0','0'};
 		char[] str2={'5','e','2'};
-		char[] str3={'3','.','1'};
-		char[] str4={'1','2','e'};
-		System.out.println(isNumeric(str1));
+		char[] str3={'1','3','.','4','5','e','+','6'};
+		char[] str4={'.','2','3'};
+		System.out.println(isNumeric(str4));
 	}
 
     public static boolean isNumeric(char[] str) {
@@ -13,9 +13,34 @@ public class nowcoder_IsNumeric{
         	return false;
         if(str[0]=='+'||str[0]=='-'){
         	char[] str1=Arrays.copyOfRange(str,1,str.length);
-            System.out.println(str1);
+            // System.out.print(str1);
+            return isnum(str1);
+        }else
+            return isnum(str);        
+    }
+
+    public static boolean isnum(char[] str){
+        if(str==null || (!Character.isDigit(str[0])&str[0]!='.'))
+            return false;
+        boolean hasE=false, decimal=false, sign=false;
+        for(int i=0;i<str.length;i++){
+            if(str[i]=='e' || str[i]=='E'){
+                if(i==str.length-1 || hasE)
+                    return false;
+                hasE=true;   
+            }else if(str[i]=='+' || str[i]=='-'){
+                if(i==str.length-1)
+                    return false;
+                if(sign || (str[i-1]!='E'&&str[i-1]!='e'))
+                    return false;
+                sign=true;
+            }else if(str[i]=='.'){
+                if(hasE || decimal)
+                    return false;
+                decimal=true;
+            }else if(str[i]<'0' || str[i]>'9')
+                return false;
         }
-        
         return true;
     }
 }
