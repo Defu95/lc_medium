bfa9fa71fb2a382bde795b37cdbde87cd86a0481
Defu95 <wangdefu95@gmail.com>
2019-03-03 00:40:58 +0800

    nowcoder

--- nowcoder_FindGreatestSumOfSubArray.java
+++ nowcoder_FindGreatestSumOfSubArray.java
@@ -9,6 +9,18 @@ public class nowcoder_FindGreatestSumOfSubArray {
         	return 0;
         if(array.length==1)
         	return array[0];
-
+        int[] dp=new int[array.length];
+        int res=array[0];
+        dp[0]=array[0];
+        for(int i=1;i<array.length;i++){
+            if(array[i]+dp[i-1]>array[i])
+                dp[i]=dp[i-1]+array[i];
+            else
+                dp[i]=array[i];
+            res=Math.max(res,dp[i]);
+        }
+        // for(int num:dp)
+        //     System.out.print(num+" ");
+        return res;
     }
 }
