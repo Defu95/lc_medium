99707cfc779e690039dddd939ea011cd5eda15ec
Defu95 <wangdefu95@gmail.com>
2019-03-04 23:11:31 +0800

    tmp

---
+++ nowcoder_maxInWindows.java
@@ -0,0 +1,6 @@
+public class nowcoder_maxInWindows {
+    public ArrayList<Integer> maxInWindows(int [] num, int size)
+    {
+        
+    }
+}
---
+++ quicksort.java
@@ -0,0 +1,12 @@
+public class quicksort{
+	public static void main(String[] args) {
+		int[] numbers={2,3,4,2,6,2,5,1};
+		sort(numbers);
+		for(int num: numbers)
+			System.out.print(num+" ");
+	}
+
+	public static void sort(int[] numbers){
+
+	}
+}
